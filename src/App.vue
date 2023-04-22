aca6624bfff451b7ae37cadd10d2d0cda47003e6
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-04-22 14:36:33 +0100

    Add project files for appointmentmanager application written in Vue.js

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main-app" class="container-fluid p-4">
+    <div class="row justify-content-center">
+      <appointment-list :appointments="appointments"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import  AppointmentList  from "./components/AppointmentList"
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      appointments: []
+    }
+  },
+  components: {
+    AppointmentList
+  },
+  mounted() {
+    axios.get("./data/appointments.json")
+          .then(response => (this.appointments = response.data))
+  }
+}
+</script>
